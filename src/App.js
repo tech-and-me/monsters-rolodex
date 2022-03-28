d923406703babd45d9c79a5cc81ef2ca3ae8d2de
Pary <81phary@gmail.com>
2022-03-28 15:12:13 +1100

    final commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import CardList from './CardList';
-import {robots} from './robots';
+import {robots} from './robots'; 
+// we use this when we dont use api so we can set all states to robots
 import SearchBox from './SearchBox';
 
 
 const App = () => {
-    const [allRobots,setAllRobots] = useState(robots);
-    const [robotsToBeDisplayed,setRobotsToBeDisplayed] = useState(robots);
+    const [allRobots,setAllRobots] = useState([]);
+    const [robotsToBeDisplayed,setRobotsToBeDisplayed] = useState([]);
+
+    useEffect ( () => {
+        (async () => {
+            let userData;
+            try{
+                const response = await fetch('https://jsonplaceholder.typicode.com/users');
+                userData = await response.json();
+                setAllRobots(userData);
+                setRobotsToBeDisplayed(userData);
+            }catch (error){
+                console.log(error);
+            }
+        })();
+    },[]);
+
+
+
 
     const searchChangeFunc = (event) => {
         const searchName = event.target.value;
@@ -18,7 +36,7 @@ const App = () => {
     }
     return (
         <div className = 'tc bg-mid-gray ma0 pt5'>
-            <h1 className='ttu f1 lh-title washed-red ma0'>My Robo Friends</h1>
+            <h1 className='ttu f1 lh-title washed-red ma0'>Monsters Rolodex</h1>
             <SearchBox searchChange={searchChangeFunc}/>
             <CardList robots={robotsToBeDisplayed}/>
         </div>
